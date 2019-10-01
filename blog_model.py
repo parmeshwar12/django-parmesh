7c63adaf271fcf0a35867837951fd3407f5876e7
parmeshwar12 <36465865+parmeshwar12@users.noreply.github.com>
2019-10-01 11:24:30 +0530

    Update blog_model.py

--- blog_model.py
+++ blog_model.py
@@ -1,1 +1,22 @@
+# make the blog in website 
+from django.db import models
+#from django.utils import timezone
+from django.contrib.auth.models import User
+
+class Entry(models.Model):
+  entry_title = models.CharField(max_length=50)
+  entry_text = models.TextField()
+  entry_date = models.DateTimeField(auto_now_add=True)
+  entry_author = models.ForeignKey(User, on_delete=models.CASCADE)
+  
+  class Meta:
+    verbose_name_plural = "entries"
+    
+   def __str__(self):
+    return self.entry_title
+  
+  
+  
+  
+
 
